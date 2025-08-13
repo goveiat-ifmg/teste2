3d58b9d364fa875364250b0933ea52498283ace5
Vixnu Nepali <70670817+nvixnu@users.noreply.github.com>
2025-08-13 16:34:47 -0300

    Melhoria no código do par ou ímpar

--- par_ou_impar.c
+++ par_ou_impar.c
@@ -4,10 +4,6 @@ int main(){
   int n;
   printf("Informe um número\n");
   scanf("%d", &n);
-  if(n % 2 == 0){
-    printf("1");
-  }else{
-    printf("0");
-  }
+  printf("%d", !(n % 2));
   return 0;
 }
